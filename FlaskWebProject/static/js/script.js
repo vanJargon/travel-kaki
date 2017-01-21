8b11ab8988aceb0a0ae477ca5a5b8e6f9b323494
vanJargon <vanessa_yun@mymail.sutd.edu.sg>
2017-01-21 17:32:33 +0800

    place autocomplete function added in preliminary search

---
+++ FlaskWebProject/static/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    function initialize(){
+        var place_options={
+            types:['(cities)']
+        };
+
+        var place_input = document.getElementById('header_input');
+        var autocomplete_place = new google.maps.places.Autocomplete(place_input,place_options);
+    }
+
+    google.maps.event.addDomListener(window,'load',initialize);
+});
